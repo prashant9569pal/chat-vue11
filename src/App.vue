bf7f9c7eed05cb96e2791d6f6cfafdf1ec28460e
PRASHANT PAL <118741353+prashant9569pal@users.noreply.github.com>
2024-03-05 12:58:22 +0530

    second commit

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="view login" v-if="state.username === '' || state.username === null">
     <form class="login-form" @submit.prevent="Login">
       <div class="form-inner">
-        <h1>Login to FireChat</h1>
+        <h1>Login to Chat-k</h1>
         <label for="username">Username</label>
         <input 
           type="text" 
